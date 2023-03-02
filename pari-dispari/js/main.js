206e4a6573c0ea34935dc13ce618c22cae131225
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-02 15:53:42 +0100

    Inserimento dei prompt

---
+++ pari-dispari/js/main.js
@@ -0,0 +1,14 @@
+
+// chiedo all'utente di scegliere pari o dispari
+const pariDispariUser = prompt('Inserisci "pari" o "dispari".');
+
+// chiedo all'utente di inserire un numero da 1 a 5
+const numberUser = parseInt(prompt("Inserisci un numero da 1 a 5."));
+
+console.log(pariDispariUser);
+console.log(numberUser);
+
+
+
+// creo la function
+
